22b79b3c03591112146434df1ca184fce42ab811
Macpiece <macpiece@Macpieces-MacBook.local>
2010-02-15 16:51:10 -0800

    permissions in place for diff groups

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -12,13 +12,18 @@ class InquiriesController < ApplicationController
     @inquiries = Inquiry.all
   end
   
+  def show
+    @inquiry = Inquiry.find(params[:id])
+    @owner = @inquiry
+  end
+  
   def create
     @inquiry = Inquiry.new(params[:inquiry])
     if !@inquiry.save
       render :action => "new"
     else
       @inquiry_page = Page.find_by_permalink!('suggestions')
-      @page = Page.find_by_permalink!('suggestion_recevied') # used in create view
+      @page = Page.find_by_permalink!('suggestion_confirm') # used in create view
       add_breadcrumb @inquiry_page.name, "/#{@inquiry_page.permalink}"
       add_breadcrumb "Thank you for your suggestion."
     end
@@ -26,10 +31,10 @@ class InquiriesController < ApplicationController
 
   def find_page
     @page = Page.find_by_permalink!('suggestions')
-    @article_categories = ArticleCategory.active
-    @article_archive = Article.published.group_by { |a| [a.published_at.month, a.published_at.year] }
-    @article_authors = Person.active.find(:all, :conditions => "articles_count > 0")
-    @article_tags = Article.published.tag_counts.sort_by(&:name)
+    # @article_categories = ArticleCategory.active
+    # @article_archive = Article.published.group_by { |a| [a.published_at.month, a.published_at.year] }
+    # @article_authors = Person.active.find(:all, :conditions => "articles_count > 0")
+    # @article_tags = Article.published.tag_counts.sort_by(&:name)
   end
 
 end
--- routes.rb
+++ routes.rb
@@ -1,7 +1,8 @@
-resources :inquiries, :as => "suggestions", :only => [:new, :create]
+resources :inquiries, :as => "suggestions", :only => [:new, :create, :show]
 
 namespace :admin do |admin|
   admin.resources :inquiries, :as => 'suggestions', :has_many => :comments
 end
 
 suggestions '/suggestions', :controller => "inquiries", :action => "new"
+suggestions_received '/suggestions_received', :controller => "inquiries", :action => "index"
