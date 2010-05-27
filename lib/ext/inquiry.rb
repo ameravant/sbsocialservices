bb7abe523d24f3f7d05466d19dc3b0c7ac2afdb8
Macpiece <macpiece@Macpieces-MacBook.local>
2010-05-27 16:08:20 -0700

    added email to inquiry

--- lib/ext/inquiry.rb
+++ lib/ext/inquiry.rb
@@ -4,7 +4,7 @@ class Inquiry < ActiveRecord::Base
   has_many :comments, :as => :commentable, :dependent => :destroy
 
   def email
-    self.email == "no_reply"
+    self.email = "no_reply"
   end
 
 end
