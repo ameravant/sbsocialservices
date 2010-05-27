a276e1bf3e94197ec0b090bc5711066690fd91a1
Macpiece <macpiece@Macpieces-MacBook.local>
2010-05-27 16:06:41 -0700

    added email to inquiry

--- lib/ext/inquiry.rb
+++ lib/ext/inquiry.rb
@@ -2,4 +2,9 @@ class Inquiry < ActiveRecord::Base
   validates_presence_of :inquiry, :message => "message can't be blank"
   default_scope :order => "created_at desc"
   has_many :comments, :as => :commentable, :dependent => :destroy
+
+  def email
+    self.email == "no_reply"
+  end
+
 end
